7cda00fc768e8de01a4c692d23d4c30772d3bc9f
MarcosLottDito <marcos.lott@dito.com.br>
2023-10-27 16:02:12 -0300

    creating Login and Profile components

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1>profile page</h1>
+      <p>name:</p>
+      <p>birth date:</p>
+      <p>email:</p>
+    </div>
+  );
+}
+
+export default Profile;
