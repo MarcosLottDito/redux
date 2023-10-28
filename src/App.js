368b6df57a30dd4699095208c28e8d820b41bcf1
MarcosLottDito <marcos.lott@dito.com.br>
2023-10-28 17:07:26 -0300

    updating style sheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Profile from "./components/Profile";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Profile />
       <Login />
     </div>
