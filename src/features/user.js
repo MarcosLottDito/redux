1f2e3f9620050770f66cf6f3a45cb5c7440f6d11
MarcosLottDito <marcos.lott@dito.com.br>
2023-10-29 16:01:46 -0300

    using useDispatch to update the user state

--- src/features/user.js
+++ src/features/user.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const userSlice = createSlice({
+const userSlice = createSlice({
   name: "user",
   initialState: { value: { name: "", age: 0, email: "" } },
   reducers: {
