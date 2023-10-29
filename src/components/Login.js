86a2fe5294677b5ac46425850280a52569b0732e
MarcosLottDito <marcos.lott@dito.com.br>
2023-10-29 15:59:00 -0300

    using useDispatch to update the user state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/user";
 
 function Login() {
+  const dispatch = useDispatch();
+
+  const user = {
+    name: "marcos",
+    age: 21,
+    email: "lott.marcos@gmail.com",
+  };
+
   return (
     <div>
-      <button>login</button>
+      <button onClick={() => dispatch(login(user))}>login</button>
     </div>
   );
 }
